9d746d0c2e1b29a82b8ac38e669a3a9974e49358
T360-v2-DTPB-s15 <hintalo68@gmail.com>
2022-03-02 16:18:28 +0100

    user

---
+++ control_loop.py
@@ -0,0 +1,10 @@
+print("Begin")
+name = "John Doe"
+print(name)
+name = "Jane Doe"
+for i in range(0, 5):
+    print(i)
+    print("hello python")
+print("End")
+
+input("Add meg a nevedet")
---
+++ name_repeter.py
@@ -0,0 +1,6 @@
+# name = input("Add meg a nevedet")
+# number = input ("Add meg hányszor írjam ki")
+# for i in range(0,int(number)):
+#     print(i + 1, name)
+for name in ["piros", "feher", "zold"]:
+    print(name)
