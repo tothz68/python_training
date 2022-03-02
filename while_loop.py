aab137db244609fb97752785a96a40b055cd0f68
T360-v2-DTPB-s15 <hintalo68@gmail.com>
2022-03-02 16:47:47 +0100

    user

---
+++ while_loop.py
@@ -0,0 +1,26 @@
+# are_you_boy = True
+# print(are_you_boy)
+# print(type(are_you_boy))
+#
+# result = 10 < 20
+# print(result)
+# result = 10 == 10
+# print (result)
+# print("alma"== "alma")
+#
+# while False:
+#     print("hello")
+
+# while True:
+#     print("végtelen ciklus")
+
+# count = 0
+# while count < 10 :
+#     print( "hello" + str(count))
+#     count = count + 1
+
+
+szam = int(input( "Adj meg egy számot"))
+while szam != 0 :
+    print(2 * szam)
+
