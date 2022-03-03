f78c991fd713022139479a230c4427a153602bd0
T360-v2-DTPB-s15 <hintalo68@gmail.com>
2022-03-03 14:29:15 +0100

    user

--- function.py
+++ function.py
@@ -1,9 +1,4 @@
 # def print_employe_data(name, age): # parameter név
 #     print( "Az alkalmazott neve", name )
 #     print( "Az alkalmazott eletkora", age )
-# print_employe_data("John", 10) # paraméter érték
-input  =
-def szamok_osszege(a, b):
-    osszeg= a + b
-    print(osszeg)
-szamok_osszege(4, 6)
+# print_employe_data("John", 10) # paraméter
